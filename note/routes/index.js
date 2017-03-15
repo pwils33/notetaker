be4ed83c10df504058dbc86f33f510cb50ad7b1f
pwils33 <wilson.kcirtap@gmail.com>
2017-03-14 22:26:09 -0600

    testing url stuff

--- note/routes/index.js
+++ note/routes/index.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/noteList', function(req, res) {
+  console.log("noteList api called\n\n");
+  var person = req.url;
+  console.log(person);
+  res.send(person);
+});
+
 /* GET home page. */
 router.get('/', function(req, res) {
   console.log("Index api called \n\n");
   res.sendFile('index.html', { root: 'public' });
-});
-
-router.get("/noteList", function(req, res) {
-  console.log("noteList api called\n\n");
-  var person = req.protocol.split("/")[2];
-  console.log("person");
-  res.send(person);
 });
 
 module.exports = router;
