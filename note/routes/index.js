f4e7cc941e9d32db7c402d9c3a524ba30243b115
pwils33 <wilson.kcirtap@gmail.com>
2017-03-14 21:51:31 -0600

    added a few routes

--- note/routes/index.js
+++ note/routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  console.log("Index api called \n\n");
+  res.sendFile('index.html', { root: 'public' });
+});
+
+router.get("/noteList", function(req, res) {
+  console.log("noteList api called\n\n");
+  var person = req.protocol.split("/")[2];
+  console.log("person");
 });
 
 module.exports = router;
