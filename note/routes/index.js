e737064832fc206c8dafebe6d955bee646822893
pwils33 <wilson.kcirtap@gmail.com>
2017-03-14 22:45:56 -0600

    added stuff to notelist endpoint

--- note/routes/index.js
+++ note/routes/index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/noteList/:name', function(req, res) {
-  console.log("noteList api called\n");
-  var person = req.url;
-  console.log(person + "\n");
-  res.send(person);
-});
-
 /* GET home page. */
 router.get('/', function(req, res) {
   console.log("Index api called\n");
   res.sendFile('index.html', { root: 'public' });
 });
+
+router.get('/noteList/:name', function(req, res) {
+  console.log("noteList api called\n");
+  var person = req.url.split("/")[2];
+  console.log(person + "\n");
+  var result = person;
+  if (person) {
+    if (!personMap.get(person)) {
+      personMap.set(person,[]);
+    }
+    result = personMap.get(person);
+  }
+  res.send(result);
+});
+
+var personMap = new Map();
 
 module.exports = router;
